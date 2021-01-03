8724dcb179b91914757105fc0e69a3c2a3dc6f20
krjiye <krjiye@DESKTOP-QF3R4GF>
2021-01-03 22:07:25 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require('./models/User.js');
+const config = require('./config/key.js');
+
+//application/s-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:1234@cluster0.zmn88.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true
 }).then(()=>console.log('몽고디비 연결'))
 .catch(err => console.log(err))
@@ -11,7 +19,21 @@ mongoose.connect('mongodb+srv://test:1234@cluster0.zmn88.mongodb.net/<dbname>?re
 
 
 app.get('/', (req, res) => {
-  res.send('안녕안녕')
+  res.send('할로~')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할 떄 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
